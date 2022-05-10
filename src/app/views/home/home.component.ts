934189262a76ed8d1871cf250d32ffacb1aad98c
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-05-09 22:35:43 -0300

    Create, Edit e Delete

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ElementDialogComponent } from 'src/app/shared/element-dialog/element-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTable } from '@angular/material/table';
 
 export interface Task {
   position: number;
@@ -31,12 +34,45 @@ const TASK_DATA: Task[] = [
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild(MatTable)   // Obtendo os filhos da tabela #table
+  table!: MatTable<any>;
   displayedColumns: string[] = ['position', 'titulo', 'descricao', 'responsavel', 'prioridade', 'data', 'situacao', 'actions'];
   dataSource = TASK_DATA;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
   }
+
+  openDialog (element: Task | null): void{
+    const dialogRef = this.dialog.open(ElementDialogComponent, {
+      width: '250px',
+      data: element === null ? {    // Validando se o elemento for nulo
+        position: null,
+        titulo: '',
+        descricao: '',
+        responsavel: '',
+        prioridade: '',
+        data: '',
+        situacao: '',
+      } : element
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // Verificando se o resultado é o mesmo quando fecha o Dialog
+      if (result !== undefined) {
+        this.dataSource.push(result);  // Adicionando o result ao Array
+        this.table.renderRows();       // Atualizando a tabela
+      }
+    });
+  }
+
+  editTask(element: Task): void {
+    this.openDialog(element);
+  }
+
+  deleteTask(position: number): void {
+    this.dataSource = this.dataSource.filter(p => p.position !== position) // Devolvendo todos os elementos que 
+  }                                                                        // sejam diferente da posição que deletamos
 
 }
